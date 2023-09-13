967ddac5b05f7e89ecabf69eaa897941bdf7339e
peruvailu kamal <126966965+peruvailukamal@users.noreply.github.com>
2023-09-13 11:30:05 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,14 @@
+def nik(e):
+    u=""
+    l=""
+    for i in e:
+        if i.isupper():
+            u=u+i
+        else:
+            l=l=i
+    print(u)
+    print(l)
+e=input()
+nik(e)
+    
+    
